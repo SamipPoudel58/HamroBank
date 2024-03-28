3878335e9ce90a7b3005cd0dd3c5845939a77a8e
Samip Poudel <sawmeep58@gmail.com>
2024-03-28 12:58:44 +0545

    feat: update user model

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    isAdmin = models.BooleanField(default=False, blank=True)
     signImg = models.ImageField(upload_to="user_signatures/", null=True, blank=True)
     username = models.CharField(max_length=150, unique=True, null=True)
 
--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "name", "phone", "password", "signImg"]
+        fields = ["id", "name", "phone", "isAdmin", "password", "signImg"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
